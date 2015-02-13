f557c16075e652094272f502435fb39c2dc69936
Joe Doliner <jdoliner@gmail.com>
2015-02-13 23:00:46 +0000

    Uci analysis is working for files.
    Now to make it in to a webserver.

--- image/map/map.go
+++ image/map/map.go
@@ -1,125 +1,109 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os/exec"
-	"strings"
-	"testing/iotest"
+	"os"
+
+	"github.com/jdoliner/uci"
+	"github.com/wfreeman/pgn"
 )
 
-func runCrafty(game string, out io.Writer) error {
-	log.Print("runCrafty")
-	{
-		c := exec.Command("/usr/games/crafty")
-		stdin, err := c.StdinPipe()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		stderr, err := c.StderrPipe()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		err = c.Start()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		fmt.Fprint(stdin, "log off\r\n")
-		fmt.Fprintf(stdin, "annotate %s wb 1-999 1 2\r\n", game)
-		fmt.Fprint(stdin, "quit\r\n")
-
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(stderr)
-		if buf.Len() != 0 {
-			log.Print("Command had output on stderr.\n Cmd: ", strings.Join(c.Args, " "), "\nstderr: ", buf)
-		}
-
-		log.Print("Waiting for crafty...")
-		err = c.Wait()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		log.Print("Done")
-	}
-
-	{
-		c := exec.Command("/bin/crafty-to-json", game+".can")
-		stdout, err := c.StdoutPipe()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		stderr, err := c.StderrPipe()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		err = c.Start()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-
-		_, err = io.Copy(out, stdout)
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(stderr)
-		if buf.Len() != 0 {
-			log.Print("Command had output on stderr.\n Cmd: ", strings.Join(c.Args, " "), "\nstderr: ", buf)
-		}
-
-		log.Print("Waiting for json conversion...")
-		err = c.Wait()
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-		log.Print("Done")
-	}
-
-	return nil
-}
+var msPerMove int = 2000
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Start")
+}
 
-	f, err := ioutil.TempFile("", "game")
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	defer f.Close()
-	//defer os.Remove(f.Name())
-	_, err = io.Copy(f, iotest.NewReadLogger("MapIn", r.Body))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	err = runCrafty(f.Name(), iotest.NewWriteLogger("MapOut", w))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), 500)
-		return
-	}
+type Score struct {
+	Position        string   `json:"position"`
+	PlayedMove      string   `json:"played-move"`
+	BestMoves       []string `json:"best-moves"`
+	PlayedMoveScore int      `json:"played-move-score"`
+	BestMoveScore   int      `json:"best-move-score"`
+	White           string   `json:"white"`
+	Black           string   `json:"black"`
+	WhiteElo        string   `json:"white-elo"`
+	BlackElo        string   `json:"black-elo"`
+	HalfMoves       int      `json:"half-moves"`
 }
 
 func main() {
-	log.Print("Listening on port 80...")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	//log.Print("Listening on port 80...")
+	//http.HandleFunc("/", handler)
+	//log.Fatal(http.ListenAndServe(":80", nil))
+	f, err := os.Open("../../data/sample/xx0005")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	ps := pgn.NewPGNScanner(f)
+
+	eng, err := uci.NewEngine("/usr/games/stockfish")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set some engine options
+	eng.SetOptions(uci.Options{
+		Hash:    128,
+		Ponder:  false,
+		OwnBook: true,
+		MultiPV: 4,
+	})
+
+	encoder := json.NewEncoder(os.Stdout)
+
+	// while there's more to read in the file
+	for ps.Next() {
+		// scan the next game
+		game, err := ps.Scan()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// print out tags
+		fmt.Println(game.Tags)
+
+		var score Score
+		// make a new board so we can get FEN positions
+		b := pgn.NewBoard()
+		for i, move := range append(game.Moves, pgn.Move{}) {
+			// set the position on the board
+			eng.SetFEN(b.String())
+			// print out FEN for each move in the game
+			resultOpts := uci.HighestDepthOnly
+			results, err := eng.Go("movetime", msPerMove, resultOpts)
+			if err != nil {
+				log.Fatal(err)
+			}
+			scoreResult := results.Results[0]
+
+			if i > 0 {
+				score.PlayedMoveScore = (-1 * scoreResult.Score)
+				encoder.Encode(score)
+				log.Print("\n")
+			}
+			if i < len(game.Moves) {
+				// make the move on the board
+				b.MakeMove(move)
+
+				// Set the score for the last move
+
+				score =
+					Score{
+						Position:      b.String(),
+						PlayedMove:    move.String(),
+						BestMoves:     scoreResult.BestMoves,
+						BestMoveScore: scoreResult.Score,
+						White:         game.Tags["White"],
+						Black:         game.Tags["Black"],
+						WhiteElo:      game.Tags["WhiteElo"],
+						BlackElo:      game.Tags["BlackElo"],
+						HalfMoves:     i,
+					}
+			}
+		}
+	}
 }
